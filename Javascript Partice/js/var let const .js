9393bc7e888e12e09febe5399aef13bee0cf6251
waqarishaaluser <user.gagawaqar@gmail.com>
2024-04-21 22:20:30 +0500

    add this file

---
+++ Javascript Partice/js/var let const .js
@@ -0,0 +1,18 @@
+var courseName ="waqarhussain"
+let courseId = 123
+const courseDpt ="computer science"
+
+
+// console.log(courseName)
+// console.log(courseId)
+// console.log(courseDpt)
+
+
+
+
+console.table([courseName,courseId,courseDpt])
+
+
+courseName="python"
+courseId=123
+courseDpt="engineer"
